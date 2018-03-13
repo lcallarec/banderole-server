b6cf0b18959c72718494c5f12571c6d0323527bb
Laurent Callarec <l.callarec@gmail.com>
2018-03-13 22:29:10 +0100

    Use a middleware for setting the content-type

--- src/http.js
+++ src/http.js
@@ -3,17 +3,19 @@ const Koa = require('koa');
 
 const start = (banderole, app, port) => {
 
-    const decide = (ctx, feature) => {
+    app.use(async (ctx, next) => {
+        await next();
         ctx.response.set('content-type', 'application/json');
+    });
+
+    const decide = (ctx, feature) => {
         ctx.body = JSON.stringify(banderole.isEnabled(feature));
     };
 
     app.use(route.get('/is-enabled/:feature', decide));
     app.use(route.get('/ping', (ctx) => {
-        ctx.response.set('content-type', 'application/json');
         ctx.body = JSON.stringify('PONG');
     }));
-
 
     return app.listen(port);
 }
